78f9d38723dab75fe7ec45f3d71b858430863261
ganchock <ganchock@yahoo.com>
2022-03-11 12:15:20 +0800

    Index.js дотор хайлтын controller хийв

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,29 @@
+require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
-search.doSearch().then((r) => console.log(r));
+/**
+ * Web app төлөв
+ * - Хайлтын query8 oj ;oy
+ * - Тухайн үзүүлж байгаа жор
+ * - Лайкалсан жорууд
+ * - Захиалж байгаа жорын орцууд
+ *  */
+
+const state = {};
+const controlSearch = async() => {
+    // 1) Вэбээс хайлтын түлхүүр үгийг гаргаж авна
+    const query = "pizza";
+    if (query) {
+        // 2) Шинээр хайлтын обьектийг үүсгэж өгнө
+        state.search = new Search(query);
+        // 3) Хайлт хийхэд зориулж дэлгэцийг UI бэлтгэнэ
+        // 4) Хайлтыг гүйцэтгэнэ
+        await state.search.doSearch();
+        // 5) Хайлтын үр дүнг үзүүлнэ.
+        console.log(state.search.result);
+    }
+};
+document.querySelector(".search").addEventListener("submit", (e) => {
+    e.preventDefault();
+    controlSearch();
+});
