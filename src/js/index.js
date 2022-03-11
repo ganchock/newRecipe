cd09f3b7a6a42ab1fe9a74f121b00d7c428ecf4d
ganchock <ganchock@yahoo.com>
2022-03-11 11:41:38 +0800

    Github-д зориулсан docs folder үүсгэж, ажилд бэлтгэв

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,4 @@
-const arr = [23, 44, 12];
+import Search from "./model/Search";
 
-let myfunc = (a) => {
-    console.log(`too: ${a}`);
-};
-const arr2 = [...arr, 44, 1223];
-myfunc(arr2[1]);
+let search = new Search("pasta");
+search.doSearch().then((r) => console.log(r));
